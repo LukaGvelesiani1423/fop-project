1312e73897c61fc46b21fd8bde41e07500ba2fda
Luka Aghlemaashvili <149136351+Luka451@users.noreply.github.com>
2024-12-28 21:08:01 +0400

    Update Algorithms.java

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,7 +1,7 @@
 public class Algorithms {
     public static void main(String[] args){
         System.out.println("altorithms");
-        for (i=0; i<5;i++){
+        for (int i = 0; i < 5; i++){
             System.out.println(i); 
         }
 
