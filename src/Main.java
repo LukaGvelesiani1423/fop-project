cdba940aeb0543a9927bd0420fdcaf879262311f
Luka Aghlemashvili <aghlemashvili2005@gmail.com>
2024-12-28 16:11:02 +0400

    I added Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is Main Classs");
+        System.out.println("this is from Luka Aghlemashvili's Branch");
     }
 }
