dbc30f704bfbcce42b03199aac0003a409641015
LukaGvelesiani1423 <gvelesianiluka321@gmail.com>
2024-12-28 16:12:38 +0400

    Update Algorithms.java

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,5 +1,9 @@
 public class Algorithms {
     public static void main(String[] args){
         System.out.println("altorithms");
+        for (i=0; i<5;i++){
+            System.out.println(i); 
+        }
+            
     }
 }
