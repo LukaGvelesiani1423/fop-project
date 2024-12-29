afef53463b9e5df2ee672e7911fc3aa5ade49e07
LukaGvelesiani1423 <gvelesianiluka321@gmail.com>
2024-12-29 22:23:46 +0400

    added tokenizer

--- src/lexer/Token.java
+++ src/lexer/Token.java
@@ -1,4 +1,18 @@
 package lexer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 public class Token {
+    public static List<String> tokenize(String code) {
+        List<String> tokens = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(code, " \t\n\r\f()+-*/%={},;", true);
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken().trim();
+            if (!token.isEmpty()) { tokens.add(token);
+            }
+        }
+        return tokens;
+    }
 }
+
