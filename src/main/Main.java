612d724be12653cb96aceaef5966c93a4ee925c4
Luka Aghlemashvili <aghlemashvili2005@gmail.com>
2024-12-29 21:10:48 +0400

    Updated Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,20 @@
 package main;
 
+import lexer.Lexer;
+import parser.ASTNode;
+import parser.Parser;
+import error.ErrorReporter;
+import interpreter.GolangInterpreter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is Main.Main Classs");
-        System.out.println("this is from Luka Aghlemashvili's Branch");
+        // Main entry point of the interpreter
+        // This class reads a Go program file, tokenizes, parses, and interprets it
+        String fileName = "examples/sum_first_n.go"; // Default file, can be passed as a command line argument
+
     }
 }
